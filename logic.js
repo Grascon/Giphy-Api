b50663d279c775d01aceb33e3f51385b0a1ae2a8
Grascon <gilberto.r.s@icloud.com>
2018-03-02 12:58:58 -0700

    Added buttons dynamically, function to create new buttons, giphy api, and css styling. New buttons do not work to create new gifs

---
+++ logic.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+	var topics = ["Dog", "Wolf", "Lion", "Fox"];
+	
+  function createButtons(){
+    $('#animalButton').empty();
+    for (var i = 0; i < topics.length; i++){
+      var newTopic = $('<button type="button" class="btn btn-default">');
+      newTopic.addClass("newButton");
+      newTopic.attr('data-name', topics[i]);
+      newTopic.text(topics[i]);
+      $('#animalButton').append(newTopic);
+    }
+  }
+
+  $('#addAnimal').on('click', function(event){
+      event.preventDefault();
+      var newTopic = $('#animal-input').val().trim();
+      topics.push(newTopic);
+      createButtons(newTopic);
+      $('#animal-input').val('');
+  
+    });
+
+  createButtons();
+	
+	$('.newButton').on('click', function(){
+      
+    	var gif = $(this).attr("data-name");
+      
+    	console.log(gif);
+    
+    	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=sBn0KqtEYhmnymUFZfTMzQVVRtggNUrF&limit=10";
+    
+    	$.ajax({
+          url: queryURL,
+          method: "GET"
+      	})
+
+      	.then(function(response) {
+      	
+      		var results = response.data;
+
+      		console.log(response);
+          $('#animals').empty();
+      		for (var i = 0; i < results.length; i++){
+
+
+      			var imgDiv = $("<div class='item'>");
+
+      			var gifRating = results[i].rating;
+
+      			var p = $('<p>').text('Rating: ' + gifRating);
+
+        		var gifImage = $("<img>");
+        
+        		gifImage.attr('src', results[i].images.fixed_height.url);
+
+        		imgDiv.prepend(gifImage);
+            imgDiv.append(p);
+        		$('#animals').prepend(imgDiv);
+        	}
+        });
+    });
+
+})
+
+   
+  
+
