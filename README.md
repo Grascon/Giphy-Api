e0efbd1d00677862e8520d07f92539c54eba8b84
Grascon <gilberto.r.s@icloud.com>
2018-03-03 19:45:10 -0700

    Added ReadME, pseudo code, fixed form function so that user cannot input blank spaces, and added play/pause functionality to gif images

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Giphy-Api
+
+In this program you can search for animal gifs by either clicking on the animals created dynamically in the beginning with JQuery or searching new animals and clicking on the button that is created when searching for a new animal. When clicking the button the images are obtained using the giphy api and javascript/jquery. Ten images will be displayed and you must click on the image to start and pause the gif.
+
+Credits for the code to be able to play/pause the gifs by clicking on them: https://stackoverflow.com/questions/44298501/how-to-pause-and-start-gif-using-jquery-ajax
--- logic.js
+++ logic.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
+//create array of topics
 	var topics = ["Dog", "Wolf", "Lion", "Fox"];
-	
+
+//dynamically create buttons of the topics array by running a for loop and making buttons of each index
+//and adding class, attribute and text. Then appending to html area of button display
   function createButtons(){
     $('#animalButton').empty();
     for (var i = 0; i < topics.length; i++){
@@ -12,21 +15,39 @@ $(document).ready(function(){
       $('#animalButton').append(newTopic);
     }
   }
-  
+
+//running function to create buttons when opening page 
   createButtons();
 
+//function to create new buttons when entering them in the form
+//this is done by obtaining the value of what is typed and adding it to the topics array and running the create 
+//buttons with the added animal
+//if blanks are submitted a new button will not be added and you will get an alert
+//the form area will become blank after entering animal
   function createNewButtons(){
       $('#addAnimal').on('click', function(event){
         var newTopic = $('#animal-input').val().trim();
+        if (newTopic == ''){
+          alert('Please type an Animal');
+          return false;
+        }
         topics.push(newTopic);
-        $('#animal-input').val('');
         createButtons();
         return false;
+        $('#animal-input').empty();
       })
   }
-  
+
+//running the createnewbuttons function
   createNewButtons();
-	
+
+//function to create new gifs using the giphy api. the attribute of data-name is stored in a variable from  button and is used to search in giphy api
+//a for loop is used to go through the results variable which is response.data.
+//the area where the gifs are displayed is emptied each time to add the new images
+//A div is created for each result as well as a img element with the src of the still image to be able to display the images.
+//the rating is also stored from the results variable in another variable
+//this is then added to the new div and added to the html area where the images and ratings will be displayed
+//a class is added to the image to later be able to start and pause the gif
   function createGifs (){
     	var gif = $(this).attr("data-name");
     	console.log(gif);
@@ -52,8 +73,11 @@ $(document).ready(function(){
         	}
         });
   }
-
+  //on click function on buttons that will call createGifs function from the giphy api
   $(document).on('click', '.newButton', createGifs);
+  
+  //on click function that will start and pause gif images; the gif class was added in the createGifs function
+  //source for this code: https://stackoverflow.com/questions/44298501/how-to-pause-and-start-gif-using-jquery-ajax
   $('body').on('click', '.gif', function() {
     var src = $(this).attr("src");
     if($(this).hasClass('playing')){
